8699a0a493dcdacea14ed3e5dc601b87907bfeb9
mihkuno <caindayjoeninyo@gmail.com>
2024-05-26 09:05:02 +0000

    added whole card detection

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Build a YOLOv9c model from pretrained weight
-model = YOLO('yolov8n.pt')
+model = YOLO('/home/justeengg/app/runs/detect/train/weights/best.pt')
 
 # Display model information (optional)
 model.info()
 
 # Train the model for 100 epochs
-results = model.train(data='/home/justeengg/app/datasets/data.yaml', epochs=100, imgsz=320, verbose=True, batch=12, device=[0,1,2,3], workers=46)
+results = model.train(data='/home/justeengg/app/datasets/data.yaml', epochs=30, imgsz=2000, verbose=True, batch=12, device=[0,1,2,3], workers=46)
 
