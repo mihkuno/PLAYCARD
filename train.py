2083309a1ced644505f25993eeb9d886990a4ee2
mihkuno <caindayjoeninyo@gmail.com>
2024-05-28 07:51:55 +0000

    added new models

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Build a YOLOv9c model from pretrained weight
-model = YOLO('/home/justeengg/app/runs/detect/blackjack-card-sm/weights/best.pt')
+model = YOLO('/home/justeengg/app/runs/detect/train2/weights/best.pt')
 
 # Display model information (optional)
 model.info()
 
 # Train the model for 100 epochs
-results = model.train(data='/home/justeengg/app/datasets/data.yaml', epochs=30, imgsz=1000, verbose=True, batch=12, device=[0,1,2,3], workers=46)
+results = model.train(data='/home/justeengg/app/datasets/data.yaml', epochs=70, imgsz=320, verbose=True, batch=12, device=[0,1,2,3], workers=46)
 
