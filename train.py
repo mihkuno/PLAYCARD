2862193068104679f853bb03e0c994edd3860ce0
mihkuno <caindayjoeninyo@gmail.com>
2024-06-01 00:44:33 +0000

    added new blackjack card model

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Build a YOLOv9c model from pretrained weight
-model = YOLO('/home/justeengg/app/runs/detect/train2/weights/best.pt')
+model = YOLO('yolov8s.pt')
 
 # Display model information (optional)
 model.info()
 
 # Train the model for 100 epochs
-results = model.train(data='/home/justeengg/app/datasets/data.yaml', epochs=70, imgsz=320, verbose=True, batch=12, device=[0,1,2,3], workers=46)
+results = model.train(data='/home/justeengg/app/datasets/data.yaml', epochs=50, imgsz=2000, verbose=True, batch=12, device=[0,1,2,3], workers=46)
 
